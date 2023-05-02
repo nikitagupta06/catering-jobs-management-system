77e29837fce10f83362f36c0074a91462e24702f
nikitagupta06 <nikitagupta9696@gmail.com>
2023-05-02 13:29:50 +0530

    Added post, put and patch methods

--- src/main/java/com/nikitagupta06/cateringjobsmanagementsystem/controller/CateringJobsController.java
+++ src/main/java/com/nikitagupta06/cateringjobsmanagementsystem/controller/CateringJobsController.java
@@ -1,5 +1,6 @@
 package com.nikitagupta06.cateringjobsmanagementsystem.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.nikitagupta06.cateringjobsmanagementsystem.model.CateringJob;
 import com.nikitagupta06.cateringjobsmanagementsystem.model.Status;
 import com.nikitagupta06.cateringjobsmanagementsystem.repository.CateringJobsRepository;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("cateringJobs")
@@ -36,5 +38,40 @@ public class CateringJobsController {
     @GetMapping("/findByStatus")
     public List<CateringJob> getCateringJobByStatus(@RequestParam Status status) {
         return cateringJobsRepository.findByStatus(status);
+    }
+
+    @PostMapping
+    @ResponseBody
+    public CateringJob createCateringJob(@RequestBody CateringJob job) {
+        return cateringJobsRepository.save(job);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public CateringJob updateCateringJob(@RequestBody CateringJob cateringJob, @PathVariable Long id){
+        if(cateringJobsRepository.existsById(id)){
+            cateringJob.setId(id);
+            return cateringJobsRepository.save(cateringJob);
+        }else {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseBody
+    public CateringJob patchCateringJob(@PathVariable Long id, @RequestBody JsonNode json) {
+        Optional<CateringJob> optionalJob = cateringJobsRepository.findById(id);
+        if (optionalJob.isPresent()) {
+            CateringJob job = optionalJob.get();
+            JsonNode menu = json.get("menu");
+            if (menu != null) {
+                job.setMenu(menu.asText());
+                return cateringJobsRepository.save(job);
+            } else {
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        }
     }
 }
